579b102b44d2a85cc733c2cd971b77c8e1839d2e
HenaPot <hena.potogija@stu.ibu.edu.ba>
2025-07-18 14:51:04 +0200

    odbrana fixed UI issue with TextInput jumpy text

--- components/BloomTextInput.tsx
+++ components/BloomTextInput.tsx
@@ -5,11 +5,11 @@ import {Ionicons} from '@expo/vector-icons';
 
 type BloomTextInputProps = TextInputProps;
 
-const BloomTextInput = ({secureTextEntry, ...restOfProps}: BloomTextInputProps) => {
+const BloomTextInput = ({secureTextEntry, style, ...restOfProps}: BloomTextInputProps) => {
   const [secureTextEntryState, setSecureTextEntryState] = useState(secureTextEntry);
   return (
     <View style={styles.input}>
-        <TextInput {...restOfProps} secureTextEntry={secureTextEntryState}/>
+        <TextInput style={[styles.textInput, style]} {...restOfProps} secureTextEntry={secureTextEntryState}/>
         {secureTextEntry ? <Ionicons name={secureTextEntryState ? 'eye-off-outline' : 'eye-outline'} onPress={() => setSecureTextEntryState(!secureTextEntryState)} size={20} style={{ padding: 10 }}></Ionicons> : null}
     </View>
   );
@@ -17,8 +17,8 @@ const BloomTextInput = ({secureTextEntry, ...restOfProps}: BloomTextInputProps) 
 
 const styles = StyleSheet.create({
     input: {
+      flex: 1,
       width: "100%",
-      height: 56,
       borderWidth: 1,
       borderColor: "#000",
       borderRadius: 5,
@@ -28,6 +28,14 @@ const styles = StyleSheet.create({
       alignItems: "center",
       justifyContent: "space-between",
     },
+    textInput: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 0,
+    color: "#000",
+    backgroundColor: "transparent",
+  },
   });
 
 export default BloomTextInput;
